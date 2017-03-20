bf055c96d437e6ebb4ee34dd2202c308444d22cb
zowiehi <zdh27@nau.edu>
2017-03-20 13:09:56 -0700

    point coordiantes loaded seperately from reflectance data

--- README.md
+++ README.md
@@ -8,4 +8,4 @@ After downloading the project, simply run `npm install` to install all necessary
 
 Run Application
 -
-To start the application server, please ensure a mongod instance with the spectral profiler data is running. Then, simply run `npm start`. You can connect to the database at the address shown in the console, which by default is set to [http://localhost:4200](http://localhost:4200)
+To start the application server, please ensure a mongod instance with the spectral profiler data is running. Then, simply run `npm start`. You can connect to the database at the address shown in the console, which by default is set to [localhost:4200](http://localhost:4200)
--- public/src/main.js
+++ public/src/main.js
@@ -1,27 +1,27 @@
 $(window).on("load", () => {
     $.getJSON('api/images', (data) => {
-      console.log(data);
       plotPoints(geoJSONLayer, data.Images)
     });
 });
 
 function plotPoints(geoJSONLayer, data) {
     // Grab the points of every element.
-    let geoDataPoints = data.map(elem =>
-        elem.pts.map(point =>
+    let geoDataPoints = data.map(elem => {
+      let e = elem._id;
+      return elem.pts.map(point =>
             ({
+                eid: elem._id,
+                index: 0,
                 type: point.loc.type,
                 coordinates: point.loc.coordinates,
-                ref1: binArray2FloatArray(point.ref1),
-                ref2: binArray2FloatArray(point.ref2)
             })
-        ))
-        .reduce((a, b) => a.concat(b), [])
+        )
+      })
+        .reduce((a, b) => a.concat(b), []);
     geoJSONLayer.addData(geoDataPoints);
 }
 
 function binArray2FloatArray(string) {
-    console.log(string);
     let byteArray = base64js.toByteArray(string);
     return new Float32Array(byteArray.buffer);
 }
