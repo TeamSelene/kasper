68007ac8178b454fedc3d37954c6adf41c53ae58
Christopher Robert Philabaum <christopher@philabaum.me>
2017-03-20 21:40:14 -0700

    Directly use index parameter for .map instead.

--- public/src/main.js
+++ public/src/main.js
@@ -1,25 +1,21 @@
 $(window).on("load", () => {
     $.getJSON('api/images', (data) => {
-      plotPoints(geoJSONLayer, data.Images)
+        plotPoints(geoJSONLayer, data.Images)
     });
 });
 
 function plotPoints(geoJSONLayer, data) {
     // Grab the points of every element.
-    let geoDataPoints = data.map(image => {
-      let i = 0;
-      return image.pts.map(point => {
-            let pt = {
-                eid: image._id,
-                index: i,
-                type: point.loc.type,
-                coordinates: point.loc.coordinates,
-            };
-            i += 1;
-            return pt;
-          }
-        );
-      })
+    let geoDataPoints = data.map(image =>
+            image.pts.map((point, i) =>
+                ({
+                    eid: image._id,
+                    index: i,
+                    type: point.loc.type,
+                    coordinates: point.loc.coordinates,
+                })
+            )
+        )
         .reduce((a, b) => a.concat(b), []);
     geoJSONLayer.addData(geoDataPoints);
 }
