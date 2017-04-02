19f2066b13c55f53f2d4055a91c6703449a9aff4
zowiehi <zdh27@nau.edu>
2017-04-02 13:40:35 -0700

    Styling and map display settings

--- server.js
+++ server.js
@@ -9,14 +9,6 @@ const api           =   require('./server/routes/api');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ "extended":false }));
 
-// app.use((req, res, next) => {
-//   req.db  = db;
-//   res.header('Access-Control-Allow-Origin', '*'); // We can access from anywhere
-//   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-//   res.header('Access-Control-Allow-Headers', 'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept');
-//     next();
-// });
-
 app.use(express.static('public'));
 
 app.use('/api', api);
