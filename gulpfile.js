01b37c37933052595ec0fd3861f8b58e1751b4bd
zowiehi <zdh27@nau.edu>
2017-04-06 16:49:11 -0700

    Add bower and gulp, make dependencies local

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,39 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
 
 // define plug-ins
-var flatten = require('gulp-flatten');
-var gulpFilter = require('gulp-filter'); // 4.0.0+
-var uglify = require('gulp-uglify');
-var minifycss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var mainBowerFiles = require('main-bower-files');
+const flatten         = require('gulp-flatten');
+const gulpFilter      = require('gulp-filter');
+const gulpConcat      = require('gulp-concat');
+const uglify          = require('gulp-uglify');
+const minifycss       = require('gulp-minify-css');
+const rename          = require('gulp-rename');
+const mainBowerFiles  = require('main-bower-files');
 
 // Define paths variables
-var dest_path =  'public';
+const dest_path =  'public/src/';
 // grab libraries files from bower_components, minify and push in /public
 gulp.task('publish-components', function() {
 
-        var jsFilter = gulpFilter('**/*.js');
-        var cssFilter = gulpFilter('**/*.css');
-        var fontFilter = gulpFilter(['**/*.eot', '**/*.woff', '**/*.svg', '**/*.ttf']);
+        let jsFilter = gulpFilter('**/*.js', {restore: true});
+        let cssFilter = gulpFilter('**/*.css', {restore: true});
 
-        return gulp.src(mainBowerFiles())
-
-        // grab vendor js files from bower_components, minify and push in /public
+        return gulp.src(mainBowerFiles().concat('**/*.js'))
         .pipe(jsFilter)
-        .pipe(gulp.dest(dest_path + '/src/js/'))
+        .pipe(gulp.dest(dest_path + '/js'))
         .pipe(uglify())
         .pipe(rename({
             suffix: ".min"
         }))
-        .pipe(gulp.dest(dest_path + '/src/js/'))
+        .pipe(gulp.dest(dest_path + '/js'))
         .pipe(jsFilter.restore)
 
         // grab vendor css files from bower_components, minify and push in /public
         .pipe(cssFilter)
-        .pipe(gulp.dest(dest_path + '/src/css'))
+        .pipe(gulp.dest(dest_path + '/css'))
         .pipe(minifycss())
         .pipe(rename({
             suffix: ".min"
         }))
-        .pipe(gulp.dest(dest_path + '/src/css'))
+        .pipe(gulp.dest(dest_path + '/css'))
         .pipe(cssFilter.restore)
 });
