91e307b6a356f7590af2fd9a8686db3dac30dcdd
Christopher Robert Philabaum <christopher@philabaum.me>
2017-04-11 11:09:50 -0700

    Added /api/image/:id/:in testing

--- test/api.test.js
+++ test/api.test.js
@@ -5,7 +5,7 @@ let app = require('../server');
 
 describe('API', () => {
     describe('Integration Tests', () => {
-        describe('GET /api/image/', () => {
+        describe('GET /api/images/', () => {
             it('should use charset UTF-8', (done) => {
                 request(app)
                     .get('/api/images')
@@ -21,6 +21,27 @@ describe('API', () => {
                     .expect('Content-Type', /application\/json/)
                     .expect(200, done);
             });
-        })
+        });
+
+        describe('GET /api/image/:id/:in', () => {
+            const id = "58c6923bff2dcd3dd1239367";
+            const index = 0;
+
+            it('should use charset UTF-8', (done) => {
+                request(app)
+                    .get(`/api/image/${id}/${index}`)
+                    .set('Accept-Charset', 'utf-8')
+                    .expect('Content-Type', /charset\=(utf|UTF)\-8/)
+                    .expect(200, done);
+            });
+
+            it('should return application/json', (done) => {
+                request(app)
+                    .get(`/api/image/${id}/${index}`)
+                    .set('Accept', 'application/json')
+                    .expect('Content-Type', /application\/json/)
+                    .expect(200, done);
+            });
+        });
     });
 });
