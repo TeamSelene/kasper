84021f61af2ace3bb2279bf4e4f1ef9cfc08b2fa
Christopher Robert Philabaum <christopher@philabaum.me>
2017-04-11 11:47:43 -0700

    Renamed /api/images to /api/points in tests

--- test/api.test.js
+++ test/api.test.js
@@ -5,10 +5,10 @@ let app = require('../server');
 
 describe('API', () => {
     describe('Integration Tests', () => {
-        describe('GET /api/images/', () => {
+        describe('GET /api/points/', () => {
             it('should use charset UTF-8', (done) => {
                 request(app)
-                    .get('/api/images')
+                    .get('/api/points')
                     .set('Accept-Charset', 'utf-8')
                     .expect('Content-Type', /charset\=(utf|UTF)\-8/)
                     .expect(200, done);
@@ -16,7 +16,7 @@ describe('API', () => {
 
             it('should return application/json', (done) => {
                 request(app)
-                    .get('/api/images')
+                    .get('/api/points')
                     .set('Accept', 'application/json')
                     .expect('Content-Type', /application\/json/)
                     .expect(200, done);
