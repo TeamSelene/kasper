43fb924884f3fed540a233a1c6135129778f9e6b
Christopher Robert Philabaum <christopher@philabaum.me>
2017-04-16 20:08:46 -0700

    Added more substantial unit tests to GET /api/points

--- test/api.test.js
+++ test/api.test.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const request = require('supertest');
+const expect = require('chai').expect;
 let app = require('../server');
 
 describe('API', () => {
@@ -15,6 +16,172 @@ describe('API', () => {
                     .set('Accept', 'application/json')
                     .expect('Content-Length', octets.toString(10))
                     .expect(200, done);
+            });
+            it('should have property \'error\'', () => {
+                return request(app)
+                    .get('/api/points')
+                    .set('Accept-Charset', 'utf-8')
+                    .set('Accept', 'application/json')
+                    .expect(200)
+                    .then(resp => {
+                        expect(resp.body).to.have.property('error');
+                    });
+            });
+            it('should have property \'Points\'', () => {
+                return request(app)
+                    .get('/api/points')
+                    .set('Accept-Charset', 'utf-8')
+                    .set('Accept', 'application/json')
+                    .expect(200)
+                    .then(resp => {
+                        expect(resp.body).to.have.property('error', 0);
+                        expect(resp.body).to.have.property('Points');
+                    });
+            });
+
+            describe("#Points", () => {
+                it('should be an array', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body.Points).to.be.an('array');
+                        });
+                });
+                it('should not be empty', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body.Points).to.not.be.empty;
+                        });
+                });
+            });
+            describe("#Points[0]", () => {
+                it('should have property \'_id\'', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body).to.have.deep.property('Points[0]._id', "58ad1e5fff2dcd0ecd984a8e");
+                        });
+                });
+                it('should have property \'pts\'', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body).to.have.deep.property('Points[0].pts');
+                        });
+                });
+            });
+            describe("#Points[0].pts", () => {
+                it('should be an array', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body.Points[0].pts).to.be.an('array');
+                        });
+                });
+                it('should not be empty', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body.Points[0].pts).to.not.be.empty;
+                        });
+                });
+                it('should have length at most 39', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body.Points[0].pts).to.have.length.most(39);
+                        });
+                });
+            });
+            describe("#Points[0].pts[0]", () => {
+                it('should have property \'loc\'', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body).to.have.deep.property('Points[0].pts[0].loc');
+                        });
+                });
+            });
+            describe("#Points[0].pts[0].loc", () => {
+                it('should have property \'coordinates\'', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body).to.have.deep.property('Points[0].pts[0].loc.coordinates');
+                        });
+                });
+                it('should have property \'type\' equal to "Points"', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body).to.have.deep.property('Points[0].pts[0].loc.type', "Point");
+                        });
+                });
+            });
+            describe("#Points[0].pts[0].loc.coordinates", () => {
+                it('should be an array', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body.Points[0].pts[0].loc.coordinates).to.be.an('array');
+                        });
+                });
+                it('should have length of exactly 2', () => {
+                    return request(app)
+                        .get('/api/points')
+                        .set('Accept-Charset', 'utf-8')
+                        .set('Accept', 'application/json')
+                        .expect(200)
+                        .then(resp => {
+                            expect(resp.body).to.have.property('error', 0);
+                            expect(resp.body.Points[0].pts[0].loc.coordinates).to.have.lengthOf(2);
+                        });
+                });
             });
         });
 
