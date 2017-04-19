bddd31f39cff84c4f41494b06b8f5c8f73cb9fd7
zowiehi <zdh27@nau.edu>
2017-04-18 19:37:30 -0700

    restructure create_layer

--- create_layer.py
+++ create_layer.py
@@ -1,58 +1,59 @@
-
-# PyMongo for MongoDB queries
-from pymongo import MongoClient
-from collections import OrderedDict
-
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
 
-# Bokeh + Datashader
-import datashader as ds
-import datashader.transfer_functions as tf
-from datashader.utils import export_image
-
-# Import pandas for dataframe support
-import pandas as pd
 import json
-
-# MatPlotLib for colors and basic plotting
-import matplotlib.pyplot as plt
-import matplotlib.colors as colors
 import numpy as np
 import warnings
 
 from database import SpectralProfiler
-
-import hashlib
-warnings.filterwarnings('ignore')
-
-def create_hash(query, projection):
-    return hashlib.sha224((json.dumps(query) + json.dumps(projection)).encode()).hexdigest()
-
-# Create Connection
-conn = SpectralProfiler("localhost", 27017)
-
-# Create some Queries with a projection
-query, projection = {}, {"loc" : True, "ref2": True}
-query2, projection2 = { "loc": { "$geoWithin": { "$center": [[160, -73], 50] }    } }, {"loc" : True, "ref2": True}
-
-# Generate a dataframe from the query
-data = conn.compute_dataframe(query, projection, field = "ref2")
-
-img = conn.compute_image(600, 300, "bone", bg=None)
-conn.save_image("test.tif")
 
 # import gsconfig stuff
 import geoserver
 import geoserver.util
 from geoserver.catalog import Catalog
 
-# connect to the catalog
-cat = Catalog("http://localhost:8080/geoserver/rest/")
+import hashlib
+warnings.filterwarnings('ignore')
 
-workspace = cat.get_workspace("selene")
-layer_name = create_hash(query, projection)
-cat.create_coveragestore(layer_name, "test.tif", workspace, overwrite=True)
+def create_hash(query, projection):
+    return hashlib.sha224((json.dumps(query).lower() + json.dumps(projection)).encode()).hexdigest()
 
-print(layer_name)
+def main():
+    read_in = sys.stdin.readlines()
+    type    = int(read_in[0])
+
+    if type == 0:
+        query = {}
+    else:
+        query = json.loads(read_in[1])
+
+    projection  = {"loc" : True, "ref2": True}
+    layer_name  = create_hash(query, projection)
+
+    # connect to the catalog
+    cat         = Catalog("http://localhost:8080/geoserver/rest/")
+    workspace   = cat.get_workspace("selene")
+    found       = cat.get_layer(layer_name)
+
+    if found:
+        print(layer_name)
+        sys.exit(0)
+
+    # Create Connection
+    conn = SpectralProfiler("localhost", 27017)
+
+    # Generate a dataframe from the query
+    data = conn.compute_dataframe(query, projection, field = "ref2")
+
+    img = conn.compute_image(600, 300, "bone", bg=None)
+    conn.save_image("test.tif")
+
+    cat.create_coveragestore(layer_name, "test.tif", workspace, overwrite=True)
+
+    print(layer_name)
+
+
+
+if __name__ == '__main__':
+    main()
