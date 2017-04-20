d7e3dec3c30ebc4c8c661d3037dd864c2ad9aa3c
zowiehi <zdh27@nau.edu>
2017-04-19 17:24:36 -0700

    Imporived create_layer structure

--- create_layer.py
+++ create_layer.py
@@ -17,43 +17,40 @@ import hashlib
 warnings.filterwarnings('ignore')
 
 def create_hash(query, projection):
-    return hashlib.sha224((json.dumps(query).lower() + json.dumps(projection)).encode()).hexdigest()
+    return hashlib.sha224((json.dumps(query) + json.dumps(projection)).encode()).hexdigest()
 
-def main():
-    read_in = sys.stdin.readlines()
-    type    = int(read_in[0])
 
-    if type == 0:
-        query = {}
-    else:
-        query = json.loads(read_in[1])
+read_in = sys.stdin.readline()
+type    = int(read_in[0])
+query   = {}
+if type == 1:
+    query = json.loads(read_in[1])
+# Create Connection
+conn = SpectralProfiler("localhost", 27017)
+# connect to the catalog
+cat = Catalog("http://localhost:8080/geoserver/rest/")
 
-    projection  = {"loc" : True, "ref2": True}
-    layer_name  = create_hash(query, projection)
+# Create some Queries with a projection
+query, projection = {}, {"loc" : True, "ref2": True}
+layer_name = create_hash(query, projection)
 
-    # connect to the catalog
-    cat         = Catalog("http://localhost:8080/geoserver/rest/")
-    workspace   = cat.get_workspace("selene")
-    found       = cat.get_layer(layer_name)
-
-    if found:
-        print(layer_name)
-        sys.exit(0)
-
-    # Create Connection
-    conn = SpectralProfiler("localhost", 27017)
-
+found = cat.get_layer(layer_name)
+print found
+if found:
+    print(layer_name)
+    sys.exit(0)
+else:
     # Generate a dataframe from the query
     data = conn.compute_dataframe(query, projection, field = "ref2")
 
     img = conn.compute_image(600, 300, "bone", bg=None)
     conn.save_image("test.tif")
 
+
+
+    workspace = cat.get_workspace("selene")
+
     cat.create_coveragestore(layer_name, "test.tif", workspace, overwrite=True)
 
     print(layer_name)
-
-
-
-if __name__ == '__main__':
-    main()
+    sys.exit(0)
